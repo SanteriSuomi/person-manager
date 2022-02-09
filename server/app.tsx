ae0dbfaeba00a0ad9f242c409cba156a2e5eedb5
Santeri Suomi <sant.suomi@gmail.com>
2022-02-09 23:13:48 +0200

    Fix server port environment variable typo. This is why heroku build was failing, as the right port was not used

--- server/app.tsx
+++ server/app.tsx
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 const app = express();
-const port = process.env.port || 8000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
@@ -13,6 +13,6 @@ app.get("/world", (req, res) => {
 	res.send("Hello World!");
 });
 
-app.listen(port, () => {
-	return console.log(`Express is listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+	return console.log(`Express is listening at http://localhost:${PORT}`);
 });
